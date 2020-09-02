eb4f6426cc34e0eb47a734fb26de3d1415f4a789
cristina <c.fujiname@gmail.com>
2020-09-02 16:51:13 +0100

    adding a class

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,7 +1,4 @@
 require 'DockingStation'
+describe DockingStation do
 
-describe 'DockingStation' do
-    it 'creates a new DockingStation when the method new is called on it' do
-        expect(DockingStation.new).to eq DockingStation.new
-    end
 end
