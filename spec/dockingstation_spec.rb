f373eae0f1bf26248ecc43b703d19f22691b9476
cristina <c.fujiname@gmail.com>
2020-09-02 17:22:14 +0100

    defined methods

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,4 +1,8 @@
 require 'DockingStation'
 describe DockingStation do
-
+  describe 'release_bike' do
+    it 'responds to releasing a bike from docking station' do
+      expect(DockingStation.new).to respond_to(:release_bike)
+    end
+  end
 end
