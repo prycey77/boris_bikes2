4c0d334f64bd659cbff9844f7d948d9a7d744d94
cristina <c.fujiname@gmail.com>
2020-09-03 14:18:00 +0100

    last exercise 8

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,7 +2,7 @@ require 'DockingStation'
 describe DockingStation do
   describe 'release_bike' do
     it 'responds to releasing a bike from docking station' do
-      expect(DockingStation.new).to respond_to(:release_bike)
+      expect(DockingStation.new).to respond_to(:release_bike) #DockingStation.new is the object of the class
     end
   end
 end
