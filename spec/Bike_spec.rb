159fd6ccc002792addd7c811b71ba283b568bcc6
Student <student@Makerss-MacBook-Air.local>
2020-09-03 14:28:58 +0100

    added Bike class and defined working? method

---
+++ spec/Bike_spec.rb
@@ -0,0 +1,10 @@
+require 'Bike'
+
+describe Bike do
+    describe 'a method that asks if a bike is working' do
+        it 'responds to the method "working?"' do
+            bike = Bike.new
+            expect(bike).to respond_to(:working?)
+        end
+    end
+end
