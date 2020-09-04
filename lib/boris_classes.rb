4e5d41bf0a9d3696eacc399073a4faae7fc04a9e
Student <student@Makerss-MacBook-Air.local>
2020-09-04 15:19:54 +0100

    Created a @bike array instance and changed some tests

--- lib/boris_classes.rb
+++ lib/boris_classes.rb
@@ -1,28 +1,28 @@
 class Bike
+  
   def working?
       true
   end
 end
 
 class DockingStation
-  attr_reader (:empty)
-  def initialize(empty=0)
-    @empty = empty
+  attr_reader :bike
+  def initialize
+    
+    @bike = []
   end
 
   def release_bike
-    if @empty == 0
-      raise Exception.new "There are no bikes."
-    else
-      Bike.new
-    end
+    fail "There are no bikes." if @bike.length == 0
+    @bike.pop
+    
   end
 
-  def dock_bike
-    if @empty == 10
+  def dock_bike(bike)
+    if @bike >= 20
       fail "This docking station is full"
-    elsif @empty < 10
-      @empty += 1
+    elsif @bike < 20
+      @bike << Bike.new
       return "The bike has been docked"
     end
   end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,30 +1,36 @@
 require 'boris_classes'
 
 describe DockingStation do
-  station = DockingStation.new(0)
+  station = DockingStation.new
   it 'responds to releasing a bike from docking station' do
     expect(DockingStation.new).to respond_to(:release_bike) #DockingStation.new is the object of the class
   end
 
-  it 'Checks to see if the bike is working' do
-  station = DockingStation.new(1)
+  #it 'Checks to see if the bike is working' do
+  #station = DockingStation.new
   # bike = station.release_bike
-  expect(station.release_bike.working?).to eq true
-  end
+  #expect(station.release_bike.working?).to eq true
+  #end
   
   it 'responds to a bike being docked' do
-    expect(DockingStation.new).to respond_to(:dock_bike)
+    expect(DockingStation.new).to respond_to :dock_bike
   end
  #it "Won't all a bike to dock if station is full" do
   #  station = DockingStation.new(10)
    # expect(station.dock_bike).to eq "There is already a bike here"
   #end
   it 'raises an error if the station is empty' do
-    station = DockingStation.new(0)
-    expect {station.release_bike}.to raise_error
+    station = DockingStation.new
+    expect {station.release_bike}.to raise_error "There are no bikes."
   end
   it 'raises an error when trying to dock a bike at a full docking station' do
-    station = DockingStation.new(10)
-    expect {station.dock_bike}.to raise_error
+    station = DockingStation.new
+    expect {station.dock_bike}.to raise_error "This docking station is full"
+  end
+  describe "#dock_bike" do
+    it 'checks that 20 bikes can be released' do
+      20.times { subject.dock Bike.new} 
+    expect {subject.dock Bike.new}.to raise_error "This docking station is full"
+    end
   end
 end
